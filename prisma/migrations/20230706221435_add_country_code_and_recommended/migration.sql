3862cac0a10e846ed1cedb7600a295a0edbc013d
GuiBarros0 <guisanches.exe@gmail.com>
2023-07-06 19:16:29 -0300

    feat: add seed script and new migration

---
+++ prisma/migrations/20230706221435_add_country_code_and_recommended/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `ImagesUrl` on the `Trip` table. All the data in the column will be lost.
+  - Added the required column `countryCode` to the `Trip` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Trip" DROP COLUMN "ImagesUrl",
+ADD COLUMN     "countryCode" TEXT NOT NULL,
+ADD COLUMN     "highlights" TEXT[],
+ADD COLUMN     "imagesUrl" TEXT[],
+ADD COLUMN     "locationDescription" TEXT NOT NULL DEFAULT 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry''s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
+ADD COLUMN     "recommended" BOOLEAN NOT NULL DEFAULT false,
+ALTER COLUMN "coverImage" SET NOT NULL,
+ALTER COLUMN "coverImage" SET DATA TYPE TEXT;
