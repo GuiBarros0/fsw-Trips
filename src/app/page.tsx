24b130e571f4279280096ce087fb336ca63e52b1
GuiBarros0 <guisanches.exe@gmail.com>
2023-07-07 14:04:59 -0300

    feet: add recomended trips components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-"use client"
-
-import QuickSearch from "./components/QuickSearch"
-import TripSearch from "./components/TripSearch"
+import QuickSearch from "./components/QuickSearch";
+import RecommendedTrips from "./components/RecommendedTrips";
+import TripSearch from "./components/TripSearch";
 
 export default function Home() {
-
   return (
     <div>
-      <TripSearch/>
-      <QuickSearch/>
+      <TripSearch />
+      <QuickSearch />
+      <RecommendedTrips />
     </div>
-  )
+  );
 }
